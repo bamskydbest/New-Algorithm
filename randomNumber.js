afa703dc9c3650a656eaa2f3f5d74912c5d1135e
User <user@Mahmoud-MacBook-Pro.local>
2025-03-13 15:27:48 +0100

    Algorithm Six

--- randomNumber.js
+++ randomNumber.js
@@ -1,3 +1,6 @@
 //Write a function randomNumber that generates a random number between two given numbers (inclusive)
 
-const randomNumber = (min, max) => {};
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log(randomNumber(1, 10));
